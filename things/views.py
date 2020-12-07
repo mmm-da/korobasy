597a0da06525f8044e6cb793c141f257d8ec87f7
spAm25 <spAm250799@gmail.com>
2020-12-07 16:53:39 +0000

    бек: добавил группировку эндпоинтов в доках апишки

--- things/views.py
+++ things/views.py
@@ -2,7 +2,11 @@ from things.models import Thing,ThingInstance,ThingCategory
 from things.serializers import ThingSerializer,ThingInstanceSerializer,ThingCategorySerializer
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from drf_spectacular import utils
 
+@utils.extend_schema(
+    tags=['Thing']
+)
 class ThingViewset(ModelViewSet):
     queryset = Thing.objects.all()
     serializer_class = ThingSerializer
@@ -10,12 +14,18 @@ class ThingViewset(ModelViewSet):
     permission_classes = (IsAuthenticated,)
 
 
+@utils.extend_schema(
+    tags=['Thing Instance']
+)
 class ThingInstanceViewset(ModelViewSet):
     queryset = ThingInstance.objects.all()
     serializer_class = ThingInstanceSerializer
 
     permission_classes = (IsAuthenticated,)
 
+@utils.extend_schema(
+    tags=['Thing Category']
+)
 class ThingCategoryViewset(ModelViewSet):
     queryset = ThingCategory.objects.all()
     serializer_class = ThingCategorySerializer
